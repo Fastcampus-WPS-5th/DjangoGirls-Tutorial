4f0a6feed41d2e439c1230afb368d6645ba69837
Lee <dev@azelf.com>
2017-05-30 15:36:02 +0900

    post_create POST request make Post

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 
+from django.contrib.auth import get_user_model
+
 from .models import Post
+
+User = get_user_model()
 
 
 def post_list(request):
     # posts변수에 ORM을 이용해서 전체 Post의 리스트(쿼리셋)를 대입
-    posts = Post.objects.all()
+    # posts = Post.objects.all()
+
+    # posts변수에 전체 Post를 최신내림차순으로 정렬한 쿼리셋을 대입
+    posts = Post.objects.order_by('-created_date')
 
     # posts변수에 ORM을 사용해서 전달할 쿼리셋이
     # Post의 published_date가 timezone.now()보다
@@ -32,7 +39,19 @@ def post_detail(request, pk):
 
 
 def post_create(request):
-    context = {
+    if request.method == 'GET':
+        context = {
 
-    }
-    return render(request, 'blog/post_create.html', context)
+        }
+        return render(request, 'blog/post_create.html', context)
+    elif request.method == 'POST':
+        data = request.POST
+        title = data['title']
+        text = data['text']
+        user = User.objects.first()
+        post = Post.objects.create(
+            title=title,
+            text=text,
+            author=user
+        )
+        return redirect('post_detail', pk=post.pk)
